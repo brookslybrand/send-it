1cb19b71bbf3f8a93bc7e6fb7ff6b232885f9e3c
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-11-21 12:25:20 -0600

    Clean up styles and add delete project button

--- app/routes/sessions/new.tsx
+++ app/routes/sessions/new.tsx
@@ -19,11 +19,23 @@ export let meta: MetaFunction = () => {
 
 export let action: ActionFunction = async ({ request }) => {
   let body = await request.formData()
+
+  // TODO: implement hidden method for progressive enhancement
+  if (request.method.toLowerCase() === 'delete') {
+    const projectId = Number(body.get('id'))
+
+    if (Number.isNaN(projectId)) {
+      return json({ message: 'No project ID provided' }, 400)
+    }
+    await prisma.project.delete({ where: { id: projectId } })
+    return json({ delete: true })
+  }
+
   const sessionId = Number(body.get('sessionId'))
   const grade = body.get('grade')
 
   if (Number.isNaN(sessionId)) {
-    return json({ message: 'No session provided' }, 400)
+    return json({ message: 'No session ID provided' }, 400)
   } else if (!isGrade(grade)) {
     return json({ message: 'Invalid grade' }, 400)
   }
@@ -177,28 +189,20 @@ function Grade({ sessionId, grade, projects }: GradeProps) {
         <button
           type="submit"
           className={clsx(
-            'text-2xl font-bold w-full flex justify-between items-center',
+            'text-3xl font-bold w-full flex justify-between items-center',
             'text-green-600 hover:text-green-800 active:text-green-900 group'
           )}
         >
           <span>{createGradeLabel(grade)}</span>
-
-          <span
-            // Note: this will break visually if there is a 3 digit number
-            className={clsx(
-              'ring-2 ring-offset-8 rounded-full ring-current leading-[2rem] w-8 h-8',
-              'group-active:text-blue-400 group-active:ring-blue-400 group-active:ring-4'
-            )}
-          >
-            {projects.length}
-          </span>
+          <ProjectIcon count={projects.length} />
         </button>
       </Form>
       <p className="pt-2 text-xl text-gray-400">Attempts</p>
       <ul>
         {projects.map(({ id, attempts }) => (
-          <li key={id}>
+          <li key={id} className="flex justify-between">
             <AttemptsControl attempts={attempts} />
+            <RemoveProjectButton projectId={id} />
           </li>
         ))}
       </ul>
@@ -250,14 +254,15 @@ function isGrade(grade: any): grade is Grade {
 
 type AttemptsControlProps = { attempts: number }
 function AttemptsControl({ attempts }: AttemptsControlProps) {
+  const atMinAttempts = attempts === 1
   return (
     <Form className="flex items-center py-4 space-x-6">
-      <button>
+      <button className="group" disabled={atMinAttempts}>
         <VisuallyHidden>decrease attempts</VisuallyHidden>
-        <MinusIcon />
+        <MinusIcon disabled={atMinAttempts} />
       </button>
       <span className="text-3xl font-semibold text-gray-700">{attempts}</span>
-      <button>
+      <button className="group">
         <VisuallyHidden>increase attempts</VisuallyHidden>
         <PlusIcon />
       </button>
@@ -265,11 +270,54 @@ function AttemptsControl({ attempts }: AttemptsControlProps) {
   )
 }
 
-function MinusIcon() {
+type RemoveProjectButtonProps = { projectId: number }
+function RemoveProjectButton({ projectId }: RemoveProjectButtonProps) {
+  return (
+    <Form method="delete">
+      {/* TODO: abstract out this method input */}
+      <input type="hidden" name="_method" value="delete" />
+      <input type="hidden" name="id" value={projectId} />
+      <button type="submit" name="butts" value="more butts" className="group">
+        <VisuallyHidden>remove project</VisuallyHidden>
+        <RemoveIcon />
+      </button>
+    </Form>
+  )
+}
+
+type IconProps = {
+  disabled?: boolean
+}
+
+function ProjectIcon({ count }: { count: number }) {
+  return (
+    <div className="relative group-active:text-blue-400">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={clsx(iconBase, 'group-active:stroke-[2px]')}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9 12m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      {/* Note: this will break visually if there is a 3 digit number */}
+      <span className="text-3xl font-semibold absolute text-center leading-[0px] w-full inset-0 top-1/2">
+        {count}
+      </span>
+    </div>
+  )
+}
+
+function MinusIcon({ disabled = false }: IconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className={iconClassName}
+      className={attemptIconClassName(disabled)}
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
@@ -277,18 +325,17 @@ function MinusIcon() {
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={1}
         d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
       />
     </svg>
   )
 }
 
-function PlusIcon() {
+function PlusIcon({ disabled = false }: IconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className={iconClassName}
+      className={attemptIconClassName(disabled)}
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
@@ -296,11 +343,36 @@ function PlusIcon() {
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={1}
         d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
       />
     </svg>
   )
 }
 
-const iconClassName = 'h-16 w-16 text-green-800 text-opacity-50'
+function RemoveIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={clsx(iconBase, 'text-red-500 group-hover:text-red-700')}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  )
+}
+
+function attemptIconClassName(disabled: boolean) {
+  return clsx(
+    iconBase,
+    'text-green-800 text-opacity-50 group-disabled:text-green-200',
+    !disabled ? 'group-hover:text-green-600 ' : ''
+  )
+}
+
+const iconBase = 'h-16 w-16 stroke-1 group-active:stroke-2'
